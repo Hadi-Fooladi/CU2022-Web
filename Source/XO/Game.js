77e08babb45e043982051abc5a64cd03e560ad4a
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-09-13 10:10:48 -0400

    BUG FIX: Draw Calculation

--- Source/XO/Game.js
+++ Source/XO/Game.js
@@ -24,7 +24,7 @@ class Game {
         if (this.IsWinner(O)) return O;
 
         for (let i = 0; i < 9; i++)
-            if (a[i] != B)
+            if (a[i] == B)
                 return Valid;
 
         return Draw;
