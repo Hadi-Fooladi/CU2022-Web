e1e84f5d7603cfc0112617c7d81ba092e067f804
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-09-26 19:22:58 -0400

    New Blank Razor Page: Notes

---
+++ Source/NoteManager/NoteManager/Pages/Notes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NoteManager.Pages
+{
+    public class NotesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
