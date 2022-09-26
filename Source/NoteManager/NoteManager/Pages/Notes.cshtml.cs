550b4c4322eac5720b229ae7ab5ef21779c74da5
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-09-26 19:29:37 -0400

    Sample View

--- Source/NoteManager/NoteManager/Pages/Notes.cshtml.cs
+++ Source/NoteManager/NoteManager/Pages/Notes.cshtml.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,13 @@ namespace NoteManager.Pages
 {
     public class NotesModel : PageModel
     {
+        public List<int> Numbers { get; set; }
+
         public void OnGet()
         {
+            Numbers = new List<int>();
+            for (int i = 1; i < 10; i++)
+                Numbers.Add(i);
         }
     }
 }
