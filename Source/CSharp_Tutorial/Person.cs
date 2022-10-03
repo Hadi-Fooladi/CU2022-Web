98cb022e217d4a13b98dd38ea7c5c544b6a9484a
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-10-03 19:08:16 -0400

    Basic Class

---
+++ Source/CSharp_Tutorial/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Tutorial
+{
+	internal class Person
+	{
+		public string FirstName;
+		public string LastName;
+
+		public string FullName
+		{
+			get
+			{
+				return $"{FirstName} {LastName}";
+			}
+		}
+
+	}
+}
---
+++ Source/CSharp_Tutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp_Tutorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var jeff = new Person();
+			jeff.FirstName = "Jeff";
+			jeff.LastName = "Bezos";
+			Console.WriteLine(jeff.FullName);
+		}
+	}
+}
