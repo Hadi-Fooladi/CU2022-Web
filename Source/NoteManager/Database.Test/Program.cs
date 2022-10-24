ed31ddffe9868f69c0ad53fdef8c0755e3deab75
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-10-24 18:10:10 -0400

    New Project: Database.Test

---
+++ Source/NoteManager/Database.Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NoteManager.DB
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var db = new Database("DB.db3");
+
+			//db.Add(new Note
+			//{
+			//	Text = "This is a test.",
+			//	InsertDate = DateTime.Now,
+			//	UpdateDate = DateTime.Now,
+			//});
+
+			//for(int i = 1; i < 10; i++)
+			//{
+			//	db.Add(new Note
+			//	{
+			//		Text = i.ToString(),
+			//		InsertDate = DateTime.Now,
+			//		UpdateDate = DateTime.Now,
+			//	});
+			//}
+
+			foreach (var note in db.SortedNotes)
+			{
+				Console.WriteLine(note.Text);
+			}
+		}
+	}
+}
