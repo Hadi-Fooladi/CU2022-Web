ba4e752462f763a8be0b389aae7d53da6143dafc
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-10-24 18:10:00 -0400

    Database (v1)

---
+++ Source/NoteManager/Database/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using SQLite;
+
+namespace NoteManager.DB
+{
+	public class Database
+	{
+		private readonly SQLiteConnection _con;
+
+		public Database(string path)
+		{
+			_con = new SQLiteConnection(path);
+			_con.CreateTable<Note>();
+		}
+
+		public void Add(Note note)
+		{
+			_con.Insert(note);
+		}
+
+		public IEnumerable<Note> Notes => _con.Table<Note>();
+		public IEnumerable<Note> SortedNotes => _con.Table<Note>().OrderBy(n => n.Text);
+	}
+}
---
+++ Source/NoteManager/Database/Note.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+
+namespace NoteManager.DB
+{
+	public class Note
+	{
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+		[Indexed]
+		public string Text { get; set; }
+
+		public DateTime InsertDate { get; set; }
+
+		public DateTime UpdateDate { get; set; }
+
+		public string Comment { get; set; }
+	}
+}
