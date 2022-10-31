7b2789cf6fa77b5755b128628c77ea52f119860c
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-10-31 17:57:53 -0400

    Ability to add notes

--- Source/NoteManager/NoteManager/Pages/AddNote.cshtml.cs
+++ Source/NoteManager/NoteManager/Pages/AddNote.cshtml.cs
@@ -1,22 +1,33 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
 using System.Collections.Generic;
 
 
 namespace NoteManager.Pages
 {
+    using DB;
+    using System;
+
     public class AddNoteModel : PageModel
     {
-        public IReadOnlyList<string> Notes { get; set; }
+        [BindProperty]
+        public string Text { get; set; }
 
-        //public void OnGet(string text, [FromServices] IWebHostEnvironment env)
-        //{
-        //    var notes = new Notes(Path.Combine(env.ContentRootPath, "Notes.txt"));
+        public void OnGet()
+        {
+        }
 
-        //    notes.Add(text);
-        //    Notes = notes.Texts;
-        //}
+        public IActionResult OnPost()
+        {
+            var now = DateTime.Now;
+            Global.Database.Add(new Note
+            {
+                Text = Text,
+                InsertDate = now,
+                UpdateDate = now
+            });
+
+            return RedirectToPage("Notes");
+        }
     }
 }
